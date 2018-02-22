f790eaeed4bc1c7fa1a43683a595f862617d13a0
Gamerverise Q J <netmail4ben@gmail.com>
2018-02-21 21:09:44 -0800

    Incremental

--- src/PhysSim/Main.java
+++ src/PhysSim/Main.java
@@ -11,6 +11,8 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    Particle[] particles;
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -19,12 +21,61 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Drawing Operations Test");
         Group root = new Group();
-        Canvas canvas = new Canvas(300, 250);
+        Canvas canvas = new Canvas(800, 400);
         GraphicsContext gc = canvas.getGraphicsContext2D();
         drawShapes(gc);
         root.getChildren().add(canvas);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+
+        particles = new Particle[]
+                {
+                        new Particle(400, 200, 4, 10, 0, 0, 80),
+                        new Particle(263, 654, 11, 6, 0, 0, 60),
+                        new Particle(652, 352, -32, -8, 0, 0, 40)
+                };
+    }
+
+    double G = 0.001;   // Pretend gravitation constant
+    double dt = 0.01;   // Time step
+
+    private void time_step() {
+        while (true) {
+            for (int i = 0; i < particles.length; i++) {
+                for (int j = i + 1; j < particles.length; j++) {
+                    Particle pi = particles[i];
+                    Particle pj = particles[j];
+
+                    double dist = pi.distance(pj);
+                    double acc = G * pj.mass / dist / dist;
+
+                    double dir_pi_pj_x = pj.x - pi.x; // x-component of the (pi -> pj) vector
+                    double dir_pi_pj_y = pj.y - pi.y; // y-component of the (pi -> pj) vector
+
+                    double ux = dir_pi_pj_x / dist;   // x-component of the unit vector in the direction (pi -> pj)
+                    double uy = dir_pi_pj_y / dist;   // y-component of the unit vector in the direction (pi -> pj)
+
+                    double ax = acc * ux;             // x-component of the acceleration times the unit vector
+                    double ay = acc * uy;             // y-component of the acceleration times the unit vector
+
+                    pi.ax += ax;
+                    pi.ay += ay;
+
+                    pj.ax += -ax;
+                    pj.ay += -ay;
+                }
+            }
+
+            for (int i = 0; i < particles.length; i++) {
+                Particle pi = particles[i];
+                pi.vx += pi.ax * dt;
+                pi.vy += pi.ay * dt;
+                pi.x += pi.vx * dt;
+                pi.y += pi.vy * dt;
+
+                // update image
+            }
+        }
     }
 
     private void drawShapes(GraphicsContext gc) {
@@ -48,5 +99,31 @@ public class Main extends Application {
                 new double[]{210, 210, 240, 240}, 4);
         gc.strokePolyline(new double[]{110, 140, 110, 140},
                 new double[]{210, 210, 240, 240}, 4);
+    }
+}
+
+class Particle {
+    double x;
+    double y;
+    double vx;
+    double vy;
+    double ax;
+    double ay;
+    double mass;
+
+    Particle(double x, double y, double vx, double vy, double ax, double ay, double mass) {
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        this.ax = ax;
+        this.ay = ay;
+        this.mass = mass;
+    }
+
+    double distance(Particle p) {
+        double dx = p.x - x;
+        double dy = p.y - y;
+        return Math.sqrt(dx*dx - dy*dy);
     }
 }
