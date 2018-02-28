ca3139d18a19810f4a5f481a68d476b12c2aeafc
Gamerverise Q J <netmail4ben@gmail.com>
2018-02-27 17:14:30 -0800

    Incremental

--- src/PhysSim/Main.java
+++ src/PhysSim/Main.java
@@ -1,104 +1,146 @@
 package PhysSim;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.ArcType;
 import javafx.stage.Stage;
+
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Main extends Application {
 
     Particle[] particles;
+    Thread sim_thread;
+
+    AnimationTimer anim_timer;
+    GraphicsContext gc;
+
+    ReentrantLock particles_pos_lock = new ReentrantLock();
+
+    double G = 6.67408e-11;    // pretend gravitation constant
+    double dt = 1;   // time step in ms
+
+    double canvas_width = 800;
+    double scale;
+    double x_offset;
+    double y_offset;
+
+    // register your screen coordinates and your simulation coordinates
+    // constant of proportionality for the masses
 
     public static void main(String[] args) {
         launch(args);
     }
 
+    Particle sun = new Particle(0, 0, 0, 0, 0, 0, 1.989e30, 6.957e8, 0);
+    Particle earth = new Particle(1.496e11, 0, 0, 3e4, 0, 0, 5.972e24, 6.371e6, 0);
+
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Drawing Operations Test");
-        Group root = new Group();
-        Canvas canvas = new Canvas(800, 400);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-        drawShapes(gc);
-        root.getChildren().add(canvas);
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+    public void start(Stage stage) throws InterruptedException {
+        double earth_acc = G * sun.mass / earth.x / earth.x;
+        earth.ax = -earth_acc;
 
-        particles = new Particle[]
-                {
-                        new Particle(400, 200, 4, 10, 0, 0, 80),
-                        new Particle(263, 654, 11, 6, 0, 0, 60),
-                        new Particle(652, 352, -32, -8, 0, 0, 40)
-                };
-    }
+        double sun_volume = 4 / 3.0 * Math.PI * Math.pow(sun.radius, 3);
+        sun.density = sun.mass / sun_volume;
 
-    double G = 0.001;   // Pretend gravitation constant
-    double dt = 0.01;   // Time step
+        double earth_volume = 4 / 3.0 * Math.PI * Math.pow(earth.radius, 3);
+        earth.density = earth.mass / earth_volume;
 
-    private void time_step() {
-        while (true) {
-            for (int i = 0; i < particles.length; i++) {
-                for (int j = i + 1; j < particles.length; j++) {
-                    Particle pi = particles[i];
-                    Particle pj = particles[j];
+        particles = new Particle[]{sun, earth};
 
-                    double dist = pi.distance(pj);
-                    double acc = G * pj.mass / dist / dist;
-
-                    double dir_pi_pj_x = pj.x - pi.x; // x-component of the (pi -> pj) vector
-                    double dir_pi_pj_y = pj.y - pi.y; // y-component of the (pi -> pj) vector
-
-                    double ux = dir_pi_pj_x / dist;   // x-component of the unit vector in the direction (pi -> pj)
-                    double uy = dir_pi_pj_y / dist;   // y-component of the unit vector in the direction (pi -> pj)
-
-                    double ax = acc * ux;             // x-component of the acceleration times the unit vector
-                    double ay = acc * uy;             // y-component of the acceleration times the unit vector
-
-                    pi.ax += ax;
-                    pi.ay += ay;
-
-                    pj.ax += -ax;
-                    pj.ay += -ay;
+        sim_thread = new Thread(() -> {
+            while (true) {
+                time_step();
+                try {
+                    Thread.sleep((long)dt);
+                } catch (InterruptedException e) {
+                    System.out.println("SimulationThread.run: InterruptedException");
                 }
             }
+        });
 
-            for (int i = 0; i < particles.length; i++) {
-                Particle pi = particles[i];
-                pi.vx += pi.ax * dt;
-                pi.vy += pi.ay * dt;
-                pi.x += pi.vx * dt;
-                pi.y += pi.vy * dt;
-
-                // update image
+        anim_timer = new AnimationTimer() {
+            public void handle(long now) {
+                redraw();
             }
-        }
+        };
+
+        Group root = new Group();
+        Canvas canvas = new Canvas(canvas_width, canvas_width / 2);
+
+        root.getChildren().add(canvas);
+        Scene scene = new Scene(root, 800, 400);
+        stage.setScene(scene);
+
+        gc = canvas.getGraphicsContext2D();
+
+        stage.setTitle("Physics Simulation");
+        stage.setResizable(false);
+
+        stage.show();
+
+        sim_thread.start();
+        anim_timer.start();
     }
 
-    private void drawShapes(GraphicsContext gc) {
-        gc.setFill(Color.GREEN);
-        gc.setStroke(Color.BLUE);
-        gc.setLineWidth(5);
-        gc.strokeLine(40, 10, 10, 40);
-        gc.fillOval(10, 60, 30, 30);
-        gc.strokeOval(60, 60, 30, 30);
-        gc.fillRoundRect(110, 60, 30, 30, 10, 10);
-        gc.strokeRoundRect(160, 60, 30, 30, 10, 10);
-        gc.fillArc(10, 110, 30, 30, 45, 240, ArcType.OPEN);
-        gc.fillArc(60, 110, 30, 30, 45, 240, ArcType.CHORD);
-        gc.fillArc(110, 110, 30, 30, 45, 240, ArcType.ROUND);
-        gc.strokeArc(10, 160, 30, 30, 45, 240, ArcType.OPEN);
-        gc.strokeArc(60, 160, 30, 30, 45, 240, ArcType.CHORD);
-        gc.strokeArc(110, 160, 30, 30, 45, 240, ArcType.ROUND);
-        gc.fillPolygon(new double[]{10, 40, 10, 40},
-                new double[]{210, 210, 240, 240}, 4);
-        gc.strokePolygon(new double[]{60, 90, 60, 90},
-                new double[]{210, 210, 240, 240}, 4);
-        gc.strokePolyline(new double[]{110, 140, 110, 140},
-                new double[]{210, 210, 240, 240}, 4);
+   public void redraw() {
+//        gc.clearRect(0, 0, 800, 400);
+
+        particles_pos_lock.lock();
+
+        for (Particle p : particles)
+            draw_particle(p, (canvas_width / 2 - 20) / earth.radius, canvas_width / 2);
+
+        particles_pos_lock.unlock();
+    }
+
+    public void time_step() {
+        for (int i = 0; i < particles.length; i++) {
+            for (int j = i + 1; j < particles.length; j++) {
+                Particle pi = particles[i];
+                Particle pj = particles[j];
+
+                double dist = pi.distance(pj);
+                double acc = G * pj.mass / dist / dist;
+
+                double dir_pi_pj_x = pj.x - pi.x; // x-component of the (pi -> pj) vector
+                double dir_pi_pj_y = pj.y - pi.y; // y-component of the (pi -> pj) vector
+
+                double ux = dir_pi_pj_x / dist;   // x-component of the unit vector in the direction (pi -> pj)
+                double uy = dir_pi_pj_y / dist;   // y-component of the unit vector in the direction (pi -> pj)
+
+                double ax = acc * ux;             // x-component of the acceleration times the unit vector
+                double ay = acc * uy;             // y-component of the acceleration times the unit vector
+
+                pi.ax += ax;
+                pi.ay += ay;
+
+                pj.ax += -ax;
+                pj.ay += -ay;
+            }
+        }
+
+        particles_pos_lock.lock();
+
+        for (int i = 0; i < particles.length; i++) {
+            Particle p = particles[i];
+            p.vx += p.ax * dt;
+            p.vy += p.ay * dt;
+            p.x += p.vx * dt;
+            p.y += p.vy * dt;
+        }
+
+        particles_pos_lock.unlock();
+    }
+
+    void draw_particle(Particle p, double scale, double x_offset, double y_offset) {
+        gc.setFill(Color.rgb(0, 0, 255, 0.5));
+        double radius = p.mass * scale;
+        gc.fillOval(p.x * scale + x_offset, p.y * scale + y_offset, mass, mass);
     }
 }
 
@@ -110,8 +152,10 @@ class Particle {
     double ax;
     double ay;
     double mass;
+    double radius;
+    double density;
 
-    Particle(double x, double y, double vx, double vy, double ax, double ay, double mass) {
+    Particle(double x, double y, double vx, double vy, double ax, double ay, double mass, double radius, double density) {
         this.x = x;
         this.y = y;
         this.vx = vx;
@@ -119,11 +163,13 @@ class Particle {
         this.ax = ax;
         this.ay = ay;
         this.mass = mass;
+        this.radius = radius;
+        this.density = density;
     }
 
     double distance(Particle p) {
         double dx = p.x - x;
         double dy = p.y - y;
-        return Math.sqrt(dx*dx - dy*dy);
+        return Math.sqrt(dx*dx + dy*dy);
     }
 }
