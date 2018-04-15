ba64c6ab8716c663963b994aaf458f0f86674a4f
dad <beekdeals@yahoo.com>
2018-04-14 21:05:10 -0700

    Still developing

--- src/lib/render/Viewport.java
+++ src/lib/render/Viewport.java
@@ -1,59 +1,89 @@
 package lib.render;
 
-import static lib.render.Viewport.AspectRatioOption.*;
 public class Viewport {
-
-    public enum AspectRatioOption {
-        SCALE_TO_FIT,
-        MATCH_WIDTH,
-        MATCH_HEIGHT
-    }
 
     // _model suffix means units in model space
 
-    public double viewport_center_x_model;
-    public double viewport_center_y_model;
-    public double viewport_width_model;
-    public double viewport_height_model;
+    public double basis_center_x_model;
+    public double basis_center_y_model;
+    public double basis_width_model;
+    public double basis_height_model;
 
-    public AspectRatioOption aspect_ratio_option;
+    public double translation_unit;
+    public double scale_unit;
+
+    public int delta_x_count;
+    public int delta_y_count;
+    public int scale_count;
 
     public Viewport() {
         // Default viewport size is zero-by-zero
         // Default viewport translation is the identity operation
 
-        set_viewport(0, 0, 0, 0);
-        aspect_ratio_option = SCALE_TO_FIT;
+        set_basis_geometry(0, 0, 0, 0);
+
+        delta_x_count = 0;
+        delta_y_count = 0;
+        scale_count = 0;
     }
 
-    public void set_viewport(double viewport_center_x_model,
-                             double viewport_center_y_model,
-                             double viewport_width_model,
-                             double viewport_height_model)
+    public void set_basis_center(double basis_center_x_model, double basis_center_y_model) {
+        this.basis_center_x_model = basis_center_x_model;
+        this.basis_center_y_model = basis_center_y_model;
+    }
+    
+    public void set_basis_dimensions(double basis_width_model, double basis_height_model) {
+        this.basis_width_model = basis_width_model;
+        this.basis_height_model = basis_height_model;
+    }
+
+    public void set_basis_geometry(double basis_center_x_model,
+                             double basis_center_y_model,
+                             double basis_width_model,
+                             double basis_height_model)
     {
-        this.viewport_center_x_model = viewport_center_x_model;
-        this.viewport_center_y_model = viewport_center_y_model;
-        this.viewport_width_model = viewport_width_model;
-        this.viewport_height_model = viewport_height_model;
+        this.basis_center_x_model = basis_center_x_model;
+        this.basis_center_y_model = basis_center_y_model;
+        this.basis_width_model = basis_width_model;
+        this.basis_height_model = basis_height_model;
     }
 
-
-    void scale_to_width(double width) {
-        double height = width / aspect_ratio;
-        set_dimensions(width, height);
+    public void translate_basis(double delta_x, double delta_y) {
+        basis_center_x_model += delta_x;
+        basis_center_y_model += delta_y;
     }
 
-    void scale_to_height(double height) {
-        double width = height * aspect_ratio;
-        set_dimensions(width, height);
+    public void scale_basis(double scale) {
+        basis_width_model *= scale;
+        basis_height_model += scale;
     }
 
-    public void set_viewport_scale(double px_per_unit_model) {
+    public void translate_basis(int delta_x_count, int delta_y_count) {
+        this.delta_x_count += delta_x_count;
+        this.delta_y_count += delta_y_count;
+    }
+
+    public void scale_basis(int scale_count) {
+        this.scale_count += scale_count;
+    }
+
+    void scale_to_width(double basis_width_model) {
+        double aspect_ratio = this.basis_width_model / this.basis_height_model;
+        double height = basis_width_model / aspect_ratio;
+        set_basis_dimensions(basis_width_model, basis_height_model);
+    }
+
+    void scale_to_height(double basis_height_model) {
+        double aspect_ratio = this.basis_height_model / this.basis_height_model;
+        double width = basis_height_model * aspect_ratio;
+        set_basis_dimensions(width, basis_height_model);
+    }
+
+    public void scale_relative(double percent) {
         gcx.px_per_unit_model = px_per_unit_model;
     }
 
-    public void set_viewport_dimensions(double width_px, double height_px) {
-        viewport_width_model = width_px;
-        viewport_height_model = height_px;
+    public void scale_relative(int scale_count) {
+        this.scale_count += scale_count;
     }
 }
--- src/lib/render/context/RenderingGraphicsContext.java
+++ src/lib/render/context/RenderingGraphicsContext.java
@@ -1,18 +1,29 @@
 package lib.render.context;
 
-import lib.javafx_api_extensions.GraphicsContextX;
 import lib.render.Viewport;
 import lib.render.device.GraphicsDevice;
+
+import static lib.render.context.RenderingGraphicsContext.AspectRatioOption.SCALE_TO_FIT;
 
 public abstract class RenderingGraphicsContext<DEVICE extends GraphicsDevice>
         extends PrimitiveGraphicsContext<DEVICE>
 {
+    public enum AspectRatioOption {
+        FIXED_GEOMETRY,
+        FIXED_SCALE_GEOMETRY,
+        SCALE_TO_FIT,           // SCALE_TO_FIT is the only option implemented so far
+        MATCH_WIDTH,
+        MATCH_HEIGHT
+    }
 
     Viewport viewport;
+
+    public AspectRatioOption aspect_ratio_option;
 
     RenderingGraphicsContext(DEVICE device, Viewport viewport) {
         super(device);
         this.viewport = viewport;
+        aspect_ratio_option = SCALE_TO_FIT;
     }
 
     // begin_render should save the transform state, and apply the viewport and device transforms so that
@@ -23,4 +34,8 @@ public abstract class RenderingGraphicsContext<DEVICE extends GraphicsDevice>
     // end_render should restore the transform state so that it is ready for the next call to begin_render
 
     public abstract void end_render();
+
+    public void set_viewport_scale(double px_per_unit_model) {
+        gcx.px_per_unit_model = px_per_unit_model;
+    }
 }
