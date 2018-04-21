bffb96e361060b0dba66f2d42d1cbbfb82a7755f
StealthMiniBoss <beekdeals@yahoo.com>
2018-04-21 13:32:33 -0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/GravityGame.java

--- src/lib/data_structures/AbstractContainer.java
+++ src/lib/data_structures/AbstractContainer.java
@@ -4,28 +4,25 @@ import lib.tokens.enums.CopyType;
 
 import java.util.Iterator;
 
-public abstract class Container<T extends Containable, D>
-    extends ContainerDataStructure
+public abstract class AbstractContainer<T extends Containable, D>
         implements Iterable<T>
 {
-    public D data_structure;
-    
-    public Container() {
-        data_structure = new_data_structure();
+    public AbstractContainer() {
+        set_data_structure(new_data_structure());
     }
 
-    public Container(D data_structure, CopyType copy_type) {
-        set_data_structure(copy_data_structure(copy_type));
+    public AbstractContainer(D data_structure) {
+        set_data_structure(data_structure);
     }
 
-    public Container(Container<T, D> data_structure, CopyType copy_type) {
+    public AbstractContainer(AbstractContainer<T, D> data_structure, CopyType copy_type) {
         Iterator<T> iter = iterator();
         while (iter.hasNext())
             add_item(iter.next());
     }
 
-    public Container<T, D> new_copy(CopyType copy_type) {
-        Container<T, D> container = new_empty();
+    public AbstractContainer<T, D> new_copy(CopyType copy_type) {
+        AbstractContainer<T, D> container = new_empty();
 
         ContainerDataStructure<T, D> data_structure = container.get_data_structure();
         Iterator<T> iter = get_data_structure().iterator();
@@ -46,26 +43,19 @@ public abstract class Container<T extends Containable, D>
 //        }
 //    }
 
-    public abstract Container<T, D> new_empty();
+    public abstract AbstractContainer<T, D> new_empty();
 
     public abstract void add_item(T item);
 
 
     public abstract D new_data_structure();
 
-    public abstract D copy_data_structure(CopyType copy_type);
-    
-    public void set_data_structure(D data_structure) {
-        this.data_structure = data_structure;
-    }
+    public abstract void set_data_structure(D data_structure);
 
-    public D get_data_structure() {
-        return data_structure;
-    }
+    public abstract D get_data_structure();
 
-    
     @SuppressWarnings("unchecked")
-    public <E> E cast_data_structure() {
-        return (E) get_data_structure();
+    public <D> D cast_data_structure() {
+        return (D) get_data_structure();
     }
 }
--- src/lib/data_structures/AbstractContainerDataStructure.java
+++ src/lib/data_structures/AbstractContainerDataStructure.java
@@ -4,21 +4,29 @@ import lib.tokens.enums.CopyType;
 
 import java.util.Iterator;
 
-public abstract class Container<T extends Containable, D>
-    extends ContainerDataStructure
-        implements Iterable<T>
-{
-    public D data_structure;
-    
-    public Container() {
-        data_structure = new_data_structure();
+import static lib.tokens.enums.CopyType.COPY_DEEP;
+import static lib.tokens.enums.CopyType.COPY_SHALLOW;
+
+public abstract class AbstractContainerDataStructure<T extends Containable> implements Iterable<T> {
+
+    public abstract void add_item(T item);
+
+//    public abstract void add_item(Containable<T> item);
+
+    public abstract <D> D cast();
+
+    public AbstractContainerDataStructure(Iterable<T> data_structure, CopyType copy_type) {
+        Iterator<T> iter = data_structure.iterator();
+
+        if (copy_type == COPY_SHALLOW)
+            while (iter.hasNext())
+                add_item(iter.next());
+        else
+            while (iter.hasNext())
+                add_item(iter.next().new_copy(COPY_DEEP));
     }
 
-    public Container(D data_structure, CopyType copy_type) {
-        set_data_structure(copy_data_structure(copy_type));
-    }
-
-    public Container(Container<T, D> data_structure, CopyType copy_type) {
+    public AbstractContainerDataStructure(Container<T, D> data_structure, CopyType copy_type) {
         Iterator<T> iter = iterator();
         while (iter.hasNext())
             add_item(iter.next());
@@ -35,7 +43,7 @@ public abstract class Container<T extends Containable, D>
 
         return container;
     }
-    
+
 //    public void copy_in(Container<T> container, CopyType copy_type) {
 //        switch (copy_type) {
 //            case COPY_SHALLOW:
@@ -48,24 +56,16 @@ public abstract class Container<T extends Containable, D>
 
     public abstract Container<T, D> new_empty();
 
-    public abstract void add_item(T item);
-
 
     public abstract D new_data_structure();
 
     public abstract D copy_data_structure(CopyType copy_type);
-    
+
     public void set_data_structure(D data_structure) {
         this.data_structure = data_structure;
     }
 
     public D get_data_structure() {
         return data_structure;
-    }
-
-    
-    @SuppressWarnings("unchecked")
-    public <E> E cast_data_structure() {
-        return (E) get_data_structure();
     }
 }
